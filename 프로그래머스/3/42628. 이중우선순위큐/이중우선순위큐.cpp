93298aaa70792c6f3ccf28c22185bdea839b116c
21yuyu99 <91944385+21yuyu99@users.noreply.github.com>
2025-08-17 17:58:30 +0900

    [level 3] Title: 이중우선순위큐, Time: 10.38 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<string> operations) {
+    set<int> st;
+    
+    for(auto op:operations){        
+        if(op[0]=='I'){
+            int num = stoi(op.substr(2));
+            st.insert(num);
+        }
+        else if(op[2] == '1'){ //max delete
+            if(!st.empty()){
+                if(st.size()>1){
+                    st.erase(--st.end());   
+                }
+                else{
+                    st.erase(st.begin());
+                }
+            }
+        }
+        else{ //min delete
+            if(!st.empty()){
+                st.erase(st.begin());   
+            }
+        }
+    }
+    vector<int> answer;
+    if(st.size()==1){
+        answer = {*st.begin(),*st.begin()};
+    }
+    else if(st.size()==0){
+        answer = {0,0};
+    }
+    else{
+        answer = {*(--st.end()),*st.begin()};
+    }
+    return answer;
+}
+//정렬이 되어있어야 함.
+// set? logn
