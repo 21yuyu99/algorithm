f78b6613219dbcd30f78a4b41a21541cfe80d36e
21yuyu99 <91944385+21yuyu99@users.noreply.github.com>
2025-10-23 23:21:02 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 0.35 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.cpp
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.cpp
@@ -3,79 +3,44 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
+int toMin(string time){
+    return stoi(time.substr(0,2))*60 + stoi(time.substr(3,2));
+}
+string toStr(int time){
+    int hh = time/60;
+    int mm = time%60;
+    string hhStr = hh<10?"0"+to_string(hh):to_string(hh);
+    string mmStr = mm<10?"0"+to_string(mm):to_string(mm);
+    return hhStr + ":" + mmStr;
+}
 string solution(int n, int t, int m, vector<string> timetable) { 
     //n번, t분 간격, 최대 m명
     string answer = "";
-    
     sort(timetable.begin(),timetable.end());
-    int hh = 9;
-    int mm = 0;
-    int cnt = 0;
+    vector<int> timetableInt;
+    for(auto x:timetable){ timetableInt.push_back(toMin(x)); };
     int idx = 0;
-    int endCnt = -1; //탑승 끝난 셔틀 번호
-    int endNum = 0; //마지막 자리 수
-    int lastHH = 0; //마지막 탑승한 크루 도착시간
-    int lastMM = 0;
-    while(cnt<n){ //마지막 셔틀이면 끝남.
-        int curNum = 0; //현재 탑승 인원
-        for(;idx<timetable.size();idx++){
-            if(curNum==m) break; //만석이면 break
-            string timeStr = timetable[idx];
-            int curH = stoi(timeStr.substr(0,2));
-            int curM = stoi(timeStr.substr(3,2));
-            //cout << curH << " " << curM << endl;
-            if(curH<hh || (curH==hh && curM<=mm)){ 
-                ++curNum;
-                lastHH = curH; //마지막 크루 탑승 시간
-                lastMM = curM;
-                
-                endCnt = cnt+1; //크루를 모두 보낸 버스 번호 기록
-                endNum = curNum; //현재 셔틀 인원 기록
-            }
-            else{
-                break;
-            }
+    int curCnt = 0; // 현재 인원
+    int curTime = 540;
+    for(int i=1;i<=n;i++){
+        curCnt = 0;
+        //cout << timetableInt[idx] << " " << curTime << "\n";
+        while(idx<timetableInt.size() && curCnt<m && curTime>=timetableInt[idx]){
+            ++curCnt;
+            ++idx;
         }
-        //cout << cnt << " " << cur_num << "\n";
-        ++cnt; //셔틀 횟수
-        if(cnt==n){ //다 못태웠을 수도 있음.
-            break;
-        }
-        //다음 셔틀시간 계산
-        mm+=t;
-        hh+=mm/60;
-        mm%=60;
+        if(i==n) break;
+        curTime+=t;
     }
-    //cout << endCnt << " " << hh << " " << mm << "\n";
-    if(endCnt<n){ //이전에 다른 크루들을 모두 태우거나 아예 못태워서. 한자리 여유가 있으면, 마지막 셔틀시간
-        //마지막 셔틀시간
-        string hhStr = hh<10?"0"+to_string(hh):to_string(hh);
-        string mmStr = mm<10?"0"+to_string(mm):to_string(mm);
-        answer = hhStr + ":" + mmStr;
+    if(curCnt==m){ //인원 초과했을 때
+        int lastM = timetableInt[idx-1];
+        int answerM = lastM-1;
+        answer = toStr(answerM);
     }
-    else if(endCnt==n){ //크루들을 다 태웠을 때
-        if(endNum < m){ //자리가 남았을 때
-            //마지막 셔틀시간
-            string hhStr = hh<10?"0"+to_string(hh):to_string(hh);
-            string mmStr = mm<10?"0"+to_string(mm):to_string(mm);
-            answer = hhStr + ":" + mmStr;
-        }
-        else{ //자리가 찼을 때는 마지막 크루 도착시간보다 빨라야 함.
-            int hhInt = lastHH;
-            int mmInt = lastMM;
-            mmInt-=1;
-            if(mmInt<0){
-                mmInt = 59;
-                hhInt-=1;
-            }
-            string hhStr = hhInt<10?"0"+to_string(hhInt):to_string(hhInt);
-            string mmStr = mmInt<10?"0"+to_string(mmInt):to_string(mmInt);
-            answer = hhStr + ":" + mmStr;
-        }
+    else{
+        answer = toStr(curTime);
     }
-    //cout <<endCnt << " " << n;
-    //if(endCnt)
+   
     return answer; //제일 늦은 도착 시간
 }
 //콘은 같은 시간 도착한 사람 중 가장 뒤에 선다.
