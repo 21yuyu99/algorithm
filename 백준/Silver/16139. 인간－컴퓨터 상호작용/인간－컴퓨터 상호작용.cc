2685c0e0ab8b1d52bca0db83f24dc2e2ee6d5835
21yuyu99 <91944385+21yuyu99@users.noreply.github.com>
2025-11-03 00:12:14 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 88 ms, Memory: 22844 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -2,24 +2,19 @@
 #include <vector>
 #include <string>
 using namespace std;
+int memo[200001][26] = {0,};
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
+    cout.tie(0);
     string s;
     cin >> s;
-    int memo[2001][26] = {0,};
     int q;
     cin >> q;
     for(int i=0;i<s.length();i++){
-        //cout << s[i] << "\n";
         for(int j=0;j<26;j++){
-            if(i==0) break;
             memo[i+1][j] = memo[i][j];
-           // cout << memo[i][j] << " ";
         }
-        //cout << "\n";
-        //cout << s[i]-'a' << " ";
         ++memo[i+1][s[i]-'a'];
     }
     for(int i=0;i<q;i++){
